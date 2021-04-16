bb46f9d7f4f71b47c72f0ceb9e800fd9c26cc4a5
Your Name <you@example.com>
2021-04-16 15:20:04 +0900

    delete src

--- src/component/Header.jsx
+++
@@ -1,7 +0,0 @@
-import "../css/App.css";
-
-function Header() {
-  return <h1>To Do List</h1>;
-}
-
-export default Header;
--- src/const/filter-state.js
+++
@@ -1,8 +0,0 @@
-/**
- * Todoリストのフィルターの状態の一覧です
- */
-export const FILTER_STATE = {
-  ALL: "All",
-  ACTIVE: "Active",
-  COMPLETED: "Completed",
-};
